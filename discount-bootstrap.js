ce5afab7fc00b23ef59f8b39eeddca18e872e336
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2019-12-13 12:01:41 -0500

    Update discount-bootstrap.js

--- discount-bootstrap.js
+++ discount-bootstrap.js
@@ -1,1 +1,25 @@
-//To come
+$(document).ready(function(){
+  //Accordion functionality, listens for click on head of a given segment
+  $('div.accordion-head').click(function () {
+    var accordionSegment = $(this);
+    var accordionBlock = accordionSegment.parent();
+    accordionBlock.children('.accordion-content').removeClass('script-display-none');
+    
+    //If it's already checked
+    if (accordionSegment.attr('aria-checked') === 'true') {
+      //Uncheck it, update the icon
+      accordionSegment.attr('aria-checked','false');
+      accordionSegment.children('i.carat-down').html("expand_less");
+      accordionBlock.children('.accordion-content').removeClass('collapsed');      
+    } else {
+      
+      //If not, check it, update the icon
+      accordionSegment.attr('aria-checked','true');
+      accordionSegment.children('i.carat-down').html("expand_more");
+      accordionBlock.children('.accordion-content').addClass('collapsed');
+      
+    }
+    //jQuery default slideToggle effect
+    accordionBlock.children('.accordion-content').slideToggle( "fast" );
+  });
+});
