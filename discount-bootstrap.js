6ef4915672cb6a6796ab682bb8d0023a462360f5
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2019-12-13 14:27:56 -0500

    Update discount-bootstrap.js

--- discount-bootstrap.js
+++ discount-bootstrap.js
@@ -2,24 +2,36 @@ $(document).ready(function(){
   //Accordion functionality, listens for click on head of a given segment
   $('div.accordion-head').click(function () {
     var accordionSegment = $(this);
-    var accordionBlock = accordionSegment.parent();
-    accordionBlock.children('.accordion-content').removeClass('script-display-none');
+    var accordionLi = accordionSegment.parent();
+    var accordionUl = accordionLi.parent();
+    var accordionBlock = accordionUl.parent();
+    
+    if (accordionBlock.children('div.accordion-head').has('div.attr('aria-checked','false')')) {
+      console.log("this worked! One of the children of this accordion is not checked");
+    } else {
+      console.log("this worked! One of the children of this accordion is checked");
+    }
+    
+    if ($('div[data-role*="page"]').has("div[data-role=header]").size() > 0)
+    alert("Has header");
+    
+    accordionLi.children('.accordion-content').removeClass('script-display-none');
     
     //If it's already checked
     if (accordionSegment.attr('aria-checked') === 'true') {
       //Uncheck it, update the icon
       accordionSegment.attr('aria-checked','false');
       accordionSegment.children('i.accordion-arrow').html("expand_less");
-      accordionBlock.children('.accordion-content').removeClass('collapsed');      
+      accordionLi.children('.accordion-content').removeClass('collapsed');      
     } else {
       
       //If not, check it, update the icon
       accordionSegment.attr('aria-checked','true');
       accordionSegment.children('i.accordion-arrow').html("expand_more");
-      accordionBlock.children('.accordion-content').addClass('collapsed');
+      accordionLi.children('.accordion-content').addClass('collapsed');
       
     }
     //jQuery default slideToggle effect
-    accordionBlock.children('.accordion-content').slideToggle( "fast" );
+    accordionLi.children('.accordion-content').slideToggle( "fast" );
   });
 });
