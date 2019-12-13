c3c1777b1a37841f966b5dc95b72f6693c118763
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2019-12-13 14:33:08 -0500

    Update discount-bootstrap.js

--- discount-bootstrap.js
+++ discount-bootstrap.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
     var accordionUl = accordionLi.parent();
     var accordionBlock = accordionUl.parent();
     
-    if (accordionBlock.children('div.accordion-head').has('div.attr('aria-checked','false')')) {
+    if (accordionBlock.children('div.accordion-head.attr("aria-checked","false")').length ) {
       console.log("this worked! One of the children of this accordion is not checked");
     } else {
       console.log("this worked! One of the children of this accordion is checked");
