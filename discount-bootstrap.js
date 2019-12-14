a90eabd3b2bfb0d81be27633128a8ec610e4c5d5
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2019-12-14 00:14:08 -0500

    Update discount-bootstrap.js

--- discount-bootstrap.js
+++ discount-bootstrap.js
@@ -5,7 +5,9 @@ $(document).ready(function(){
     var accordion = accordionSegment.parents('.accordion');
     var checkedSegments = accordion.find('div[aria-checked=false]');
     var checkedLi = checkedSegments.parent();
+    
     accordionLi.children('.accordion-content').removeClass('script-display-none');   
+    
     if (accordion.hasClass("single-select")) {
       accordion.find('.accordion-head').not(accordionLi.children('.accordion-head')).attr('aria-checked','true');
       accordion.find('.accordion-content').addClass('collapsed');
@@ -17,15 +19,24 @@ $(document).ready(function(){
     }
     function accordionToggle() {
       if (accordionSegment.attr('aria-checked') === 'true') {
-        accordionSegment.attr('aria-checked','false');
-        accordionSegment.children('i.accordion-arrow').html("expand_less");
-        accordionLi.children('.accordion-content').removeClass('collapsed').slideToggle( "fast" );; 
+        accordionToggleOpen(accordionSegment);
       } else {
-        //If not, check it, update the icon
-        accordionSegment.attr('aria-checked','true');
-        accordionSegment.children('i.accordion-arrow').html("expand_more");
-        accordionLi.children('.accordion-content').addClass('collapsed').slideToggle( "fast" );
+        accordionToggleClose(accordionSegment);
       }
+    }
+    function accordionToggleOpen(segment) {
+      var currentSegment = segment;
+      var currentLi = currentSegment.parent();
+      currentSegment.attr('aria-checked','false');
+      currentSegment.children('i.accordion-arrow').html("expand_less");
+      currentLi.children('.accordion-content').removeClass('collapsed').slideToggle( "fast" );
+    }
+    function accordionToggleClose(segment) {
+      var currentSegment = segment;
+      var currentLi = currentSegment.parent();
+      currentSegment.attr('aria-checked','true');
+      currentSegment.children('i.accordion-arrow').html("expand_more");
+      currentLi.children('.accordion-content').addClass('collapsed').slideToggle( "fast" );
     }
   });
 });
